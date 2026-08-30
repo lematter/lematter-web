541cdf6334c9fb0ff4b054a2c363d43cefac09ee
heyhrudu <heyhrudu@outlook.com>
2026-08-30 22:35:14 +0530

    chore: scaffold console page component folders with index barrels

---
+++ components/console/pages/chat/index.ts
@@ -0,0 +1,6 @@
+export * from "./new";
+export * from "./chats";
+export * from "./connectors";
+export * from "./skills";
+export * from "./memory";
+export * from "./plugins";
---
+++ components/console/pages/lab/index.ts
@@ -0,0 +1,4 @@
+export * from "./overview";
+export * from "./compliance";
+export * from "./workflows";
+export * from "./reports";
